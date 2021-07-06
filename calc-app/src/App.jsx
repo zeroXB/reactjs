367d7a9c0c9be7b2fac75592c3a70b40bfc819ea
jose arturo <japereze@bancoazteca.com>
2021-07-06 18:49:06 -0500

    se llega a la clase 35

---
+++ calc-app/src/App.jsx
@@ -0,0 +1,12 @@
+//Importacion 
+import React from 'react';
+
+
+//Generacion de la funcion flecha emacscript 6
+const App = () => {
+    //Cuerpo de la funcion lo que ejecuta la funcion 
+    return <h1>Calculadora - App</h1>
+};
+
+//Exportacion 
+export default App;
---
+++ calc-app/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+//importar el componente
+import App from './App';
+
+//utilizar el componente
+ReactDom.render(<App/>, 
+    document.getElementById('root'));
